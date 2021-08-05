3b6bd49d3fec3246eec0904de9e6dfc50831818f
varun patel <varunpatel1985@gmail.com>
2021-08-06 00:47:27 +1000

    Build Angular/Bootstrap PremiumCalculator

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatepickerModule, BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot(),
+    DatepickerModule.forRoot(),
+    AccordionModule.forRoot() 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/calculator/calculator.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  calculatorForm: FormGroup ;
+  bsConfig: Partial<BsDatepickerConfig>;
+ 
+  calculatedPremium = 0;
+
+  occupationList: any = ['Cleaner', 'Doctor', 'Author', 'Farmer','Mechanic','Florist']
+ 
+
+
+  constructor(private fb: FormBuilder) { 
+    this.bsConfig = {
+      containerClass: 'theme-blue',
+      dateInputFormat: 'DD MMMM YYYY'
+    }
+
+    this.calculatorForm = this.fb.group({
+      name: ['',Validators.required],
+      age: ['',Validators.required],    
+      dateOfBirth: ['',Validators.required],
+      occupation: ['',Validators.required],
+      deathSumInsured: ['',Validators.required]
+  
+
+    })
+  }
+
+  ngOnInit(): void {
+  
+  }
+
+
+
+
+   calculatePremium()
+   {
+     let occupationRatingFactor
+
+     switch (this.calculatorForm.get('occupation')?.value) {
+       
+         case "Cleaner":
+          case "Florist":
+              occupationRatingFactor = 1.50;
+              break;
+          case "Doctor":
+              occupationRatingFactor = 1.00;
+              break;
+          case "Author":
+              occupationRatingFactor = 1.25;
+              break;
+          case "Farmer":
+          case "Mechanic":
+              occupationRatingFactor = 1.75;
+              break;
+
+          default:
+              occupationRatingFactor = 0.00;
+              break;
+     }
+
+     this.calculatedPremium = (this.calculatorForm.get('deathSumInsured')?.value * occupationRatingFactor * this.calculatorForm.get('age')?.value) / 1000 * 12;
+
+   }
+
+
+
+
+}
